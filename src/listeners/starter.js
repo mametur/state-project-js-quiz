f5e478ca7804f8eb96bb632f20f005457761875c
Pavel Bidenko <pavelbidenko2018@gmail.com>
2020-08-20 22:06:22 +0200

    fix

--- src/listeners/starter.js
+++ src/listeners/starter.js
@@ -10,11 +10,7 @@ document.getElementById('start_button').addEventListener('click', startQuiz);
 
 /*Add Question */
 
-document.getElementById('add').addEventListener('click', addNewQuestions)
+document.getElementById('add').addEventListener('click', addNewQuestions);
 
 /*Remove question*/
-const buttonsArray = [...document.querySelectorAll('button')];
-const removeBtn = buttonsArray.find((item) => item.innerHTML === 'Remove this question');
-if (removeBtn !== null) {
-    removeBtn.addEventListener('click', removeQuestionHandler)
-};
+document.getElementById('remove').addEventListener('click', removeQuestionHandler);
