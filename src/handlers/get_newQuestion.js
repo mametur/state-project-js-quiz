2d38cca59b47c13b39793554c2e51669c4a22912
Mamé <mameazad.turgut@gmail.com>
2020-08-22 10:35:50 +0200

    sources will open on the another page

--- src/handlers/get_newQuestion.js
+++ src/handlers/get_newQuestion.js
@@ -2,26 +2,27 @@
 
 console.log('--- loading handler: get newQuestion.js');
 debugger;
-let tes=document.createElement('a');
+let tes = document.createElement('a');
 let questionCounter = 0;
 let availableQuesions = [];
 let currentQuestion = {};
 const question = document.querySelector('#question');
 const button = document.querySelectorAll('#btn');
-const aEl=document.getElementById('learn-more');
+const aEl = document.getElementById('learn-more');
 
 
 function getNewQuestion() {
     availableQuesions = [...state.questions];
     if (availableQuesions.length === 0 || questionCounter >= availableQuesions.length) {
-    return;
+        return;
     };
     currentQuestion = availableQuesions[questionCounter];
     question.innerText = currentQuestion.text;
     button.forEach(button => {
-    const number = button.dataset["number"];
-    button.innerHTML = currentQuestion.answers[number];
-    aEl.href=currentQuestion.resource;
+        const number = button.dataset["number"];
+        button.innerHTML = currentQuestion.answers[number];
+        aEl.href = currentQuestion.resource;
+        aEl.target = '_blank';
     });
-    questionCounter++; 
+    questionCounter++;
 };
